170fcf8c9ede2c9d929566176e7f835382d68f2c
duonghoangdung <dung.duonghoang94@gmail.com>
2022-08-05 09:48:47 +0700

    user menu update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -59,7 +59,7 @@ const Sidebar:React.FC<Props> = ({isMenuActive, onMenuToggle, ...props}) => {
 			timeout={300}
 			classNames="slide-x"
 		>
-			<div className={`${ !isMenuActive && 'hidden'} md:flex flex-col bg-[#fcfcfc] fixed top-0 left-0 z-50 h-full w-full md:w-80`} {...props}>
+			<div className={`${ isMenuActive ? 'flex' : 'hidden'} md:flex flex-col bg-[#fcfcfc] fixed top-0 left-0 z-50 h-full w-full md:w-80`} {...props}>
 				<div className="md:hidden px-6 shadow-[inset_0_-1px_0_#f4f4f4]">
 					<button className="flex items-center justify-center text-[#6f767e] p-3" onClick={() => onMenuToggle?.()}>
 						<SVGIcon name="close" w={24} h={24} />
@@ -79,6 +79,12 @@ const Sidebar:React.FC<Props> = ({isMenuActive, onMenuToggle, ...props}) => {
 					</div>
 					{menu && <SidebarMenu menu={menu} />}
 					<div className="mt-auto mb-0">
+						<div className="flex items-center md:hidden p-3 bg-[#f4f4f4] rounded-xl mb-4">
+							<div className="mr-3">
+								<img src="//via.placeholder.com/48x48.png" alt="" className="rounded-full w-12 h-12 object-cover"/>
+							</div>
+							<div>VO THI PHUONG</div>
+						</div>
 						<SwitchBox />
 					</div>
 				</div>
